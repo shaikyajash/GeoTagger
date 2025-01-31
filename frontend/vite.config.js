9db8bed15a801919bb4e5918c10adb97b015ff6e
shaikyajash <shaikyajash@gmail.com>
2025-01-31 14:24:16 +0530

    Changes to manage routing in vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
-
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: "/",  // Ensures correct asset paths
+  build: {
+    outDir: "dist",  
+  }
 })
